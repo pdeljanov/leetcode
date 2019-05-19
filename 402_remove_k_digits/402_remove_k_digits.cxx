9bafeb90913a4f8639f9f6a7502a4322dd86b8c6
Philip Deljanov <philip.deljanov@gmail.com>
2019-05-19 17:58:51 -0400

    Add solution for #402 Remove K Digits.
    Runtime: 4 ms, faster than 99.20% of C++ online submissions.
    Memory Usage: 9.2 MB, less than 60.20% of C++ online submissions.

---
+++ 402_remove_k_digits/402_remove_k_digits.cxx
@@ -0,0 +1,66 @@
+// Copyright (c) 2018-2019 Philip Deljanov
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+
+        string ans;
+        ans.push_back(num[0]);
+
+        // Append a character that evaluates to less than '0' to allow for the final
+        // character to be checked. We use '/' as the sentinel value.
+        num.push_back('/');
+
+        // Digits in the final answer should be increasing in value from the most
+        // significant digit to the least significant digit. Treat the answer string,
+        // ans, as a stack. If the next digit in num, num[i], is less-than the last digit
+        // in ans, pop off the last digit in ans and repeat the check. Once num[i] is
+        // greater-than the last digit in ans, push that digit onto ans and continue
+        // moving forward.
+        for(int i = 1; i < num.size(); ++i) {
+            if(num[i] < ans.back() && k > 0) {
+                ans.pop_back();
+                --i;
+                --k;
+            }
+            // Do not append leading zeros.
+            else if(num[i] != '0' || ans.size() > 0) {
+                ans.push_back(num[i]);
+            }
+        }
+
+        // The last character is not a digit and will be the sentinel character '/'.
+        // Remove it.
+        ans.pop_back();
+
+        // If there are no more digits append a '0'.
+        if(ans.size() == 0) {
+            ans.push_back('0');
+        }
+
+        return ans;
+    }
+};
+
