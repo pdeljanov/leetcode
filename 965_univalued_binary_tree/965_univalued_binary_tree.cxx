02cfb72cdb2b4e4d3b991ffa1fc0abb18026e45c
Philip Deljanov <philip.deljanov@gmail.com>
2019-06-20 21:25:30 -0400

    Add solution for #965 Univalued Binary Tree.
    Runtime: 4 ms, faster than 85.65% of C++ online submissions.

---
+++ 965_univalued_binary_tree/965_univalued_binary_tree.cxx
@@ -0,0 +1,42 @@
+// Copyright (c) 2018-2019 Philip Deljanov
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+
+    bool isUnivalTree(TreeNode* node, int value) {
+        if(!node) return true;
+        if(node->val != value) return false;
+
+        return isUnivalTree(node->left, value) && isUnivalTree(node->right, value);
+    }
+
+    bool isUnivalTree(TreeNode* root) {
+        if(!root) return true;
+        return isUnivalTree(root, root->val);
+    }
+};
