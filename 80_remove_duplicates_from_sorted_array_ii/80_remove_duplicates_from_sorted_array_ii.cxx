8f20ab679b288c5e2c168bf264ba63a235320189
Philip Deljanov <philip.deljanov@gmail.com>
2019-08-15 18:04:56 -0400

    Add solution for #80 Remove Duplicates from Sorted Array II.
    Runtime: 4 ms, faster than 100.00% of C++ online submissions.

---
+++ 80_remove_duplicates_from_sorted_array_ii/80_remove_duplicates_from_sorted_array_ii.cxx
@@ -0,0 +1,52 @@
+// Copyright (c) 2018-2019 Philip Deljanov
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size() < 3) {
+            return nums.size();
+        }
+
+        size_t j = 2;
+
+        for(size_t i = 2; i < nums.size(); ++i) {
+            // The next value is not a duplicate of the previous value, copy the new value.
+            if(nums[i] > nums[j-1]) {
+                nums[j++] = nums[i];
+            }
+            // The next value is the same as the previous value, but the second-last previous
+            // value is not the same, so a duplicate is allowed, copy the duplicate value.
+            else if(nums[j-1] > nums[j-2]) {
+                nums[j++] = nums[i];
+            }
+            // Skip if the last two previous values are the same.
+            else {
+                continue;
+            }
+        }
+
+        return j;
+    }
+};
