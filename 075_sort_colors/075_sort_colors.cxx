69b4c8960c7cfb9f74d590af5dee392c4f9bc957
Philip Deljanov <philip.deljanov@gmail.com>
2019-08-15 17:06:32 -0400

    Add solution for #75 Sort Colors.
    Runtime: 0 ms, faster than 100.00% of C++ online submissions.

---
+++ 075_sort_colors/075_sort_colors.cxx
@@ -0,0 +1,51 @@
+// Copyright (c) 2018-2019 Philip Deljanov
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        size_t a = 0, b = 0, c = 0;
+        size_t ca = 0, cb = 0, cc = 0;
+
+        for(size_t i = 0; i < nums.size(); ++i) {
+            switch(nums[i]) {
+                case 0:
+                    ca++;
+                    nums[a++] = 0;
+                    cb > 0 ? nums[b++] = 1 : b++;
+                    cc > 0 ? nums[c++] = 2 : c++;
+                    break;
+                case 1:
+                    cb++;
+                    nums[b++] = 1;
+                    cc > 0 ? nums[c++] = 2 : c++;
+                    break;
+                case 2:
+                    cc++;
+                    nums[c++] = 2;
+                    break;
+            }
+        }
+    }
+};
